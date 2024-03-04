69935dfeabc392e3e87f4f9dc60e436846daf538
OlegAleksashov <fairp88@tut.by>
2024-03-04 15:29:08 +0300

    added count of completed tasks

--- src/redux/componets/Header/Header.jsx
+++ src/redux/componets/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./Header.css";
 import { useSelector } from "react-redux";
+import "./Header.css";
 
 const Header = () => {
   const tasks = useSelector((state) => state.tasklist);
   let dateTemp = new Date().toString().split(" ");
   const date = dateTemp[1] + " " + dateTemp[2] + " " + dateTemp[3];
+
+  const completedTasksCount = tasks.filter((task) => task.isCompleted).length;
 
   return (
     <header>
@@ -13,7 +15,7 @@ const Header = () => {
       <span>TODO LIST APP</span>
       <div className="list">
         <div className="date">Total amount of tasks: {tasks.length}</div>
-        <div className="date">Completed tasks: {}</div>
+        <div className="date">Completed tasks: {completedTasksCount}</div>
       </div>
     </header>
   );
